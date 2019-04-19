0722ef2c87558a077271cbdea7a1a741f688312b
AVAVT <itsavavt@gmail.com>
2019-04-19 19:23:12 +0700

    zzz

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/og1-demo/precache-manifest.2680b554bf9dbc1b55d9d6860d67c43d.js"
+  "/og1-demo/precache-manifest.c2961a099e9a5cbe5ddbbf61c2a3d7a8.js"
 );
 
 workbox.clientsClaim();
